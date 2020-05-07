5c9fda4cfcbfced22ad8a7b5e3103f4697d1e5c2
Melissa Perez <melissaperezg.96@gmail.com>
2020-05-07 14:26:07 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
-def reverse_each_word(greeting)
-  array = greeting.split(" ")
-  jin = array.collect do |w|
-    w.reverse 
-  end
-  jin.join(" ")
+def reverse_each_word(sentence)
+  first = sentence.split(" ")
+  second = first.collect {|words| words.reverse}
+  second.join(" ")
 end
